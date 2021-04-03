88ffaa171d9fde96a4fbbee874a525776ddd22f7
Iqbal Tawakkal <iqbaltawakkal1@gmail.com>
2021-04-03 23:56:54 +0700

    remove unecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Nav from "./components/Nav";
-
 import Home from "./views/Home";
-import About from "./views/About";
 import NotFound from "./views/NotFound";
 
 const App = () => {
   return (
     <Router>
-      <Nav />
       <Switch>
         <Route exact path="/">
           <Home />
-        </Route>
-        <Route path="/about">
-          <About />
         </Route>
         <Route>
           <NotFound />
