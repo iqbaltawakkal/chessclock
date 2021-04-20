ebbe0f3304acd0cd2758435e2c5e10c270c81d2a
Iqbal Tawakkal <iqbaltawakkal1@gmail.com>
2021-04-20 15:51:08 +0700

    classname props switch custom

--- src/components/SwitchCustom.jsx
+++ src/components/SwitchCustom.jsx
@@ -4,27 +4,28 @@ import PropTypes from "prop-types";
 
 const SwitchCustom = (props) => {
   return (
-    <div className="flex items-center justify-center p-12">
-      <Switch.Group as="div" className="flex items-center space-x-4">
-        <Switch.Label>{props.label}</Switch.Label>
-        <Switch
-          as="button"
-          checked={props.value}
-          onChange={props.onChange}
-          className={`${
-            props.value ? "bg-blue-600" : "bg-gray-200"
-          } relative inline-flex flex-shrink-0 h-6 w-12 transition-colors duration-200 ease-in-out border-2 border-transparent rounded-full cursor-pointer focus:outline-none focus-shadow-outline`}
-        >
-          {({ checked }) => (
-            <span
-              className={`${
-                checked ? "translate-x-6" : "translate-x-0"
-              } inline-block w-5 h-5 transition duration-200 ease-in-out transform bg-white rounded-full`}
-            />
-          )}
-        </Switch>
-      </Switch.Group>
-    </div>
+    <Switch.Group
+      as="div"
+      className={`flex justify-between ${props.className}`}
+    >
+      <Switch.Label>{props.label}</Switch.Label>
+      <Switch
+        as="button"
+        checked={props.value}
+        onChange={props.onChange}
+        className={`${
+          props.value ? "bg-blue-600" : "bg-gray-200"
+        } relative inline-flex flex-shrink-0 h-6 w-12 transition-colors duration-200 ease-in-out border-2 border-transparent rounded-full cursor-pointer focus:outline-none focus-shadow-outline`}
+      >
+        {({ checked }) => (
+          <span
+            className={`${
+              checked ? "translate-x-6" : "translate-x-0"
+            } inline-block w-5 h-5 transition duration-200 ease-in-out transform bg-white rounded-full`}
+          />
+        )}
+      </Switch>
+    </Switch.Group>
   );
 };
 
@@ -32,6 +33,7 @@ SwitchCustom.propTypes = {
   label: PropTypes.string,
   value: PropTypes.bool.isRequired,
   onChange: PropTypes.func,
+  className: PropTypes.string,
 };
 
 export default SwitchCustom;
