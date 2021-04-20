68eb1cbedc25def9ae2deb8479ce57d860e6d5ed
Iqbal Tawakkal <iqbaltawakkal1@gmail.com>
2021-04-20 15:51:32 +0700

    modal update

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,47 +1,175 @@
-import React, { useContext, useEffect, useState } from "react";
-import InputCustom from "./InputCustom";
+import React, { Fragment, useContext, useEffect, useState } from "react";
 import SwitchCustom from "./SwitchCustom";
 import { AppContext } from "../App";
+import PropTypes from "prop-types";
+import { MdClose } from "react-icons/md";
+import { Transition, RadioGroup } from "@headlessui/react";
 
-const Modal = () => {
-  const [input, setInput] = useState("test");
-  const [enable, setEnable] = useState(false);
+const options = [
+  {
+    title: "1 Min",
+    subtitle: "",
+    timer: 60000,
+    increment: 0,
+  },
+  {
+    title: "3 Min",
+    timer: 180000,
+    increment: 0,
+  },
+  {
+    title: "5 Min",
+    timer: 300000,
+    increment: 0,
+  },
+  {
+    title: "1 + 1",
+    timer: 60000,
+    increment: 1000,
+  },
+  {
+    title: "2 + 1",
+    timer: 120000,
+    increment: 1000,
+  },
+  {
+    title: "3 + 2",
+    timer: 180000,
+    increment: 2000,
+  },
+  {
+    title: "10 Min",
+    timer: 600000,
+    increment: 0,
+  },
+  {
+    title: "30 Min",
+    timer: 1800000,
+    increment: 0,
+  },
+  {
+    title: "Custom",
+  },
+];
+
+const Modal = (props) => {
   const context = useContext(AppContext);
+  const [darkMode, setDarkMode] = useState(context.state.darkMode);
+  const [flipTimer, setFlipTimer] = useState(context.state.flipTimer);
+  const [showContent, setShowContent] = useState(false);
+  const [selected, setSelected] = useState(options[0]);
 
   useEffect(() => {
-    console.log({ input, enable });
-  }, [input, enable]);
+    context.dispatch({ type: "darkMode", payload: darkMode });
+  }, [darkMode]);
 
   useEffect(() => {
-    document.body.classList.add("overflow-hidden");
-    return () => {
-      document.body.classList.remove("overflow-hidden");
-    };
-  }, []);
+    context.dispatch({ type: "flipTimer", payload: flipTimer });
+  }, [flipTimer]);
+
+  useEffect(() => {
+    context.dispatch({ type: "increment", payload: selected.increment });
+    context.dispatch({ type: "timerTop", payload: selected.timer });
+    context.dispatch({ type: "timerBottom", payload: selected.timer });
+  }, [selected]);
+
+  useEffect(() => {
+    if (props.show) {
+      document.body.classList.add("overflow-hidden");
+      setTimeout(() => {
+        setShowContent(true);
+      }, 100);
+    }
+  }, [props.show]);
+
+  const close = () => {
+    document.body.classList.remove("overflow-hidden");
+    setShowContent(false);
+    setTimeout(() => {
+      context.dispatch({ type: "isModalActive", payload: false });
+    }, 200);
+  };
 
   return (
-    <div
-      className="fixed z-50 bottom-0 left-0 right-0 w-full overflow-auto flex top-0 bg-gray-600 bg-opacity-40"
-      onClick={() =>
-        context.dispatch({ type: "isModalActive", payload: false })
-      }
-    >
-      <div className="modal fixed h-auto max-w-full bottom-0 right-0 left-0 bg-white overflow-hidden rounded-t-lg">
-        <div className="mx-auto max-w-wrapper overflow-x-auto w-full">
-          <div className="h-80 bg-white">
-            <InputCustom
-              value={input}
-              type="text"
-              label="text input"
-              placeholder="input"
-              onChange={(e) => setInput(e.target.value)}
-            ></InputCustom>
-            <SwitchCustom value={enable} onChange={() => setEnable(!enable)} />
-          </div>
+    <>
+      {props.show && (
+        <div className="fixed z-50 top-0 bottom-0 left-0 right-0 w-full overflow-auto">
+          <Transition
+            as={Fragment}
+            show={showContent}
+            enter="transform duration-300"
+            enterFrom="max-h-0"
+            enterTo="max-h-full"
+            leave="transform duration-300"
+            leaveFrom="max-h-full"
+            leaveTo="max-h-0"
+          >
+            <div className="fixed h-auto max-w-full bottom-0 right-0 left-0 bg-white dark:bg-gray-300 overflow-hidden">
+              <div className="mx-auto max-w-lg overflow-x-auto w-full min-h-screen px-4 space-y-4 ">
+                <div className="flex border-b border-gray-300 py-4 ">
+                  <button onClick={() => close()}>
+                    <MdClose className="text-2xl mr-2"> </MdClose>
+                  </button>
+                  <p className="text-xl"> Preference</p>
+                </div>
+
+                <RadioGroup value={selected} onChange={setSelected}>
+                  <div className="grid grid-cols-3 gap-3">
+                    {options.map((option) => (
+                      <RadioGroup.Option
+                        key={option.name}
+                        value={option}
+                        className={({ active, checked }) =>
+                          `${
+                            active
+                              ? "ring-2 ring-offset-2 ring-offset-gray-300 ring-white ring-opacity-60"
+                              : ""
+                          }
+                  ${
+                    checked
+                      ? "bg-gray-500 bg-opacity-75 text-white"
+                      : "bg-white border border-gray-300"
+                  }
+                    relative h-24 flex items-center justify-center rounded-lg shadow-md px-5 py-4 cursor-pointer focus:outline-none`
+                        }
+                      >
+                        {({ checked }) => (
+                          <>
+                            <RadioGroup.Label
+                              as="p"
+                              className={`font-medium text-center ${
+                                checked ? "text-white" : "text-gray-900"
+                              }`}
+                            >
+                              {option.title}
+                            </RadioGroup.Label>
+                          </>
+                        )}
+                      </RadioGroup.Option>
+                    ))}
+                  </div>
+                </RadioGroup>
+                <SwitchCustom
+                  label="Dark Mode"
+                  value={darkMode}
+                  onChange={() => setDarkMode(!darkMode)}
+                />
+                <SwitchCustom
+                  label="Flip Timer"
+                  value={flipTimer}
+                  onChange={() => setFlipTimer(!flipTimer)}
+                />
+              </div>
+            </div>
+          </Transition>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
+};
+
+Modal.propTypes = {
+  show: PropTypes.bool.isRequired,
 };
 
 export default Modal;
