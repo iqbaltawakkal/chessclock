315ee75c54151017ea39f6e5b74611c47764ed95
Iqbal Tawakkal <iqbaltawakkal1@gmail.com>
2021-04-20 15:53:00 +0700

    flip timer on time props

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -15,7 +15,7 @@ const Time = (props) => {
     <>
       <svg
         className={`font-digital text-gray-600 fill-current w-full h-full transform ${
-          props.timerFacing == "left" ? `rotate-90` : `-rotate-90`
+          props.flipTimer ? `rotate-90` : `-rotate-90`
         }`}
         viewBox="0 0 50 50"
       >
@@ -30,7 +30,7 @@ const Time = (props) => {
 
 Time.propTypes = {
   time: PropTypes.number.isRequired,
-  timerFacing: PropTypes.string,
+  flipTimer: PropTypes.bool,
 };
 
 export default Time;
